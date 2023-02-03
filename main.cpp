19e087f25928d7f17ac72633602a2565f5048e5f
ifizmat <ifizmat@gmail.com>
2023-02-03 08:52:13 +0400

    4. Property string color.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 class Pen {
-	
+	public:
+	  std::string color;
 };
 
 int main() {
 	using namespace std;
 	cout << "Test v4.01: OK.\n";
 	Pen pen;
+	pen.color = "#FFFFFF";
+	cout << "pen.color = " << pen.color << endl;
 	return 0;
 }
